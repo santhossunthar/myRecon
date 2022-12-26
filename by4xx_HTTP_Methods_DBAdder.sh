f9e7ed3b5b8be0b840a6f92f6e001d1bdf2b65a1
Santhosh <santhoshsunthar@gmail.com>
2022-12-26 18:14:22 +0530

    HTTP Methods requests

---
+++ by4xx_HTTP_Methods_DBAdder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo mysql $3 -e "create table if not exists by4xx_HTTP_Methods(URL VARCHAR(255) NOT NULL, GET INT(4) NOT NULL, POST INT(4) NOT NULL, HEAD INT(4) NOT NULL, OPTIONS INT(4) NOT NULL, PUT INT(4) NOT NULL, TRACE INT(4) NOT NULL, TRACK INT(4) NOT NULL, CONNECT INT(4) NOT NULL, PATCH INT(4) NOT NULL)"
+
+tail -n +11 $1 | head -n 9 | tee targets/$2/by4xx_HTTP_Methods.txt
+
+GET=$(sed '1q;d' targets/$2/by4xx_HTTP_Methods.txt | awk '{print $3}')
+POST=$(sed '2q;d' targets/$2/by4xx_HTTP_Methods.txt | awk '{print $3}')
+HEAD=$(sed '3q;d' targets/$2/by4xx_HTTP_Methods.txt | awk '{print $3}')
+OPTIONS=$(sed '4q;d' targets/$2/by4xx_HTTP_Methods.txt | awk '{print $3}')
+PUT=$(sed '5q;d' targets/$2/by4xx_HTTP_Methods.txt | awk '{print $3}')
+TRACE=$(sed '6q;d' targets/$2/by4xx_HTTP_Methods.txt | awk '{print $3}')
+TRACK=$(sed '7q;d' targets/$2/by4xx_HTTP_Methods.txt | awk '{print $3}')
+CONNECT=$(sed '8q;d' targets/$2/by4xx_HTTP_Methods.txt | awk '{print $3}')
+PATCH=$(sed '9q;d' targets/$2/by4xx_HTTP_Methods.txt | awk '{print $3}')
+
+sudo mysql $3 -e "insert into by4xx_HTTP_Methods (URL, GET, POST, HEAD, OPTIONS, PUT, TRACE, TRACK, CONNECT, PATCH) values ('$4', $GET, $POST, $HEAD, $OPTIONS, $PUT, $TRACE, $TRACK, $CONNECT, $PATCH)"
--- recon.sh
+++ recon.sh
@@ -6,10 +6,6 @@ if [ -z $1 ]; then
 	exit
 fi
 
-#if [[ -f "targets" ]]; then
-#    mkdir targets
-#fi
-
 mkdir -p targets/$1
 
 echo "[+] starting sublist3r..."
@@ -31,5 +27,26 @@ cat targets/$1/subdomains_alive_temp.txt | sed 's/\x1b\[[0-9;]*m//g' | anew targ
 rm targets/$1/subdomains_alive_temp.txt
 
 ./subdomains_alive_DBAdder.sh targets/$1/subdomains_alive.txt $2
+
+sudo mysql $2 -e "select subdomain from alive" | tee targets/$1/subdomains_alive_data_temp.txt 
+
+tail -n +2 targets/$1/subdomains_alive_data_temp.txt | tee targets/$1/subdomains_alive_data.txt
+
+rm targets/$1/subdomains_alive_data_temp.txt
+
+while read line
+do
+
+tools/byp4xx/bypass.sh $line | tee >(sed $'s/\033[[][^A-Za-z]*m//g' > targets/$1/by4xx_output_temp.txt)
+./by4xx_HTTP_Methods_DBAdder.sh targets/$1/by4xx_output_temp.txt $1 $2 $line
+
+done < "targets/$1/subdomains_alive_data.txt"
+
+rm targets/$1/by4xx_output_temp.txt
+
+
+
+
+
 
 
