faf261967dc94bc0c39788eca876fecac1735b60
Santhosh <santhoshsunthar@gmail.com>
2023-01-05 17:44:20 +0530

    Changes are added in linkfinder.sh

--- linkfinder.sh
+++ linkfinder.sh
@@ -2,8 +2,13 @@
 
 while read url
 do
-python3 tools/LinkFinder/linkfinder.py -i $url -d -o cli | anew targets/$2/linkfinder_output.txt
+setOfLinks=$(python3 tools/LinkFinder/linkfinder.py -i $url -d -o cli)
+
+echo "$setOfLinks" | grep '^http' | anew targets/$2/relative_URLs_temp.txt
+echo "$setOfLinks" | grep -v '^http' | sed "s|^|$url\/|1" | anew targets/$2/relative_URLs_temp.txt
 done < "$1"
+
+cat relative_URLs.txt | grep -v 'Running against:' | tee targets/$2/relative_URLs.txt
 
 cat targets/$2/linkfinder_output.txt | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|svg)" | tee targets/$2/linkfinder_output_excluded.txt
 rm targets/$2/linkfinder_output.txt
@@ -11,6 +16,13 @@ rm targets/$2/linkfinder_output.txt
 grep 'js' targets/$2/linkfinder_output_excluded.txt | tee targets/$2/JS_links_temp.txt
 cat targets/$2/JS_links_temp.txt | grep "Running against:" | awk '{print $3}' | tee targets/$2/JS_links.txt
 cat targets/$2/JS_links_temp.txt | grep -v "Running against:" | anew targets/$2/JS_links.txt
+
+grep 'http' cat targets/$2/JS_links.txt
+
+while read link
+do
+
+done < "targets/$2/JS_links.txt"
 
 grep -v 'js' targets/$2/linkfinder_output_excluded.txt | tee targets/$2/links_without_JS_links.txt
 
@@ -27,4 +39,3 @@ while read line
 do
 sudo mysql $3 -e "insert into links (link) values ('$line')"
 done < "targets/$2/links_without_JS_links.txt"
-
