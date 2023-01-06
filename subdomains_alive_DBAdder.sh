a596b77a7f1f0e69acbb632ac883848ae3a0ebb3
Santhosh <santhoshsunthar@gmail.com>
2023-01-06 21:37:42 +0530

    Removed square brackets

--- subdomains_alive_DBAdder.sh
+++ subdomains_alive_DBAdder.sh
@@ -6,9 +6,9 @@ while read line
 do 
 
 subDomain=$(echo $line | awk '{print $1}')
-statusCode=$(echo $line | awk '{print $2}')
-contentType=$(echo $line | awk '{print $3}')
-title=$(echo $line | awk '{print $4}')
+statusCode=$(echo $line | awk '{print $2}' | sed 's|[][]||g')
+contentType=$(echo $line | awk '{print $3}' | sed 's|[][]||g')
+title=$(echo $line | awk '{print $4}' | sed 's|[][]||g')
 
 sudo mysql $2 -e "insert into alive (subdomain, status_code, content_type, title) values ('$subDomain', '$statusCode', '$contentType', '$title')"
 
