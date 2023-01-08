8287bebe5d1f1967ed1c79f70afdc2b382a68eb0
Santhosh <santhoshsunthar@gmail.com>
2023-01-08 21:54:20 +0530

    Added urls for by4xx

--- bypass.sh
+++ bypass.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+sudo mysql $2 -e "select subdomain from subdomain_alive where status_code>400 and status_code<500" | tee targets/$1/subdomains_bypass_temp.txt 
+tail -n +2 targets/$1/subdomains_bypass_temp.txt | anew targets/$1/bypass.txt
+rm targets/$1/subdomains_bypass_temp.txt
+
+sudo mysql $2 -e "select url from url_alive where status_code>400 and status_code<500" | tee targets/$1/urls_bypass_temp.txt 
+tail -n +2 targets/$1/urls_bypass_temp.txt | anew targets/$1/bypass.txt
+rm targets/$1/urls_bypass_temp.txt
+
 while read line
 do
 tools/byp4xx/bypass.sh $line | tee >(sed $'s/\033[[][^A-Za-z]*m//g' > targets/$1/by4xx_output_temp.txt)
 ./by4xx_HTTP_Methods_DBAdder.sh targets/$1/by4xx_output_temp.txt $1 $2 $line
 
-done < "targets/$1/subdomains_alive_data.txt"
+done < "targets/$1/bypass.txt"
 
 rm targets/$1/by4xx_output_temp.txt
